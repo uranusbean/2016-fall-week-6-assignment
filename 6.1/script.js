78b324d4c801bdf26eb05f629efeb8afa54749df
viztech <siqi.zhu@gmail.com>
2016-10-10 21:32:01 -0400

    First commit

---
+++ 6.1/script.js
@@ -0,0 +1,18 @@
+console.log('6.1');
+
+//First, append <svg> element and implement the margin convention
+var m = {t:50,r:50,b:50,l:50};
+var outerWidth = document.getElementById('canvas').clientWidth,
+    outerHeight = document.getElementById('canvas').clientHeight;
+var w = outerWidth - m.l - m.r,
+    h = outerHeight - m.t - m.b;
+
+var plot = d3.select('.canvas')
+    .append('svg')
+    .attr('width',outerWidth)
+    .attr('height',outerHeight)
+    .append('g')
+    .attr('transform','translate(' + m.l + ',' + m.t + ')');
+
+//Import data and parse
+
