6d959be428534bffdf4b683063f467d07a795bb4
Yixuan Zhang <janicezhang.me@gmail.com>
2016-10-22 03:00:01 +0000

    modified axis white

--- 6.1/script.js
+++ 6.1/script.js
@@ -15,30 +15,30 @@ var plot = d3.select('.canvas')
     .attr('transform','translate(' + m.l + ',' + m.t + ')');
 
 //Import data and parse
-// d3.csv('../data/olympic_medal_count.csv',parse,dataLoaded);
 d3.csv('../data/olympic_medal_count.csv',parse,function(err,rows){
     console.table(rows);
     var maxCount = d3.max(rows, function(d){return d.count2012;}),
         minCount = d3.min(rows, function(d){return d.count2012;});
-    // console.log(minCount,maxCount);
     var scaleX = d3.scaleLinear(),
         scaleY = d3.scaleLinear();
     
-    scaleY.domain([minCount, maxCount])
-        .range([0,h/2]);
+    scaleY.domain([minCount, maxCount+20])
+        .range([h,0]);
     scaleX.domain([0, rows.length])
         .range([0,w]);
         
-    var yAxisLable = [0,10,20,30,40,50,60,70,80,90,100,110,120];
+    // var yAxisLable = [0,10,20,30,40,50,60,70,80,90,100,110,120];
     
-    yAxisLable.forEach(function(yLable){
-        plot.append('text')
-            .text(yLable.toString())
-            .attr('x', 0)
-            .attr('y',300-scaleY(yLable))
-            .attr('text-anchor','end');
-    });
+    // yAxisLable.forEach(function(yLable){
+    //     plot.append('text')
+    //         .text(yLable.toString())
+    //         .attr('x', 0)
+    //         .attr('y',300-scaleY(yLable))
+    //         .attr('text-anchor','end');
+    // });
     
+   
+        
     rows.sort(function(a,b){
       return b.count2012 - a.count2012;
     });
@@ -49,26 +49,21 @@ d3.csv('../data/olympic_medal_count.csv',parse,function(err,rows){
     rows.forEach(function(country, index){
         plot.append('rect')
             .attr("x", m.l+ index * 100)
-            .attr("y", 300-scaleY(country.count2012))
+            .attr("y", scaleY(country.count2012))
             .attr("width", 40)
-            .attr("height",scaleY(country.count2012) );
+            .attr("height",h-scaleY(country.count2012) );
         
         plot.append('text')
             .attr('x',index * 100 + m.l + m.r/2)
-            .attr('y',330)
+            .attr('y',h+20)
             .text(country.countryName)
             .attr('text-anchor','middle');
-            
-    for(var y = 0; y <= h; y += 20){
-        plot.append('line').attr('class','axis')
-            .attr('x1',0)
-            .attr('x2',w*2)
-            .attr('y1',y)
-            .attr('y2',y);
-    }
-
+    }) 
     
-    })  
+     var axisY = d3.axisLeft()
+        .scale(scaleY)
+        .tickSize(-w);
+    plot.append('g').attr('class','axis axis-y').call(axisY);
 });
 
 function parse(d){
@@ -77,3 +72,40 @@ function parse(d){
         count2012:(+d['2012'])? (+d['2012']):undefined
   }
 }
+    //new 
+    // var countries = plot.selectAll('.country')
+    //     .dataa(rows.slice(0,5))
+    //     .enter()
+    //     .append('g')
+    //     .attr('class','country')
+    //     .attr('transform',function(d,i){
+    //         return 'translate('+scaleX(i)+',0)';
+    //     });
+        
+    // countries 
+    //     .append('rect')
+    //     .attr('x',-20)
+    //     .aatr('y',function(d){
+    //         return scaleY(d.country2012);
+    //     })
+    //     .attr('width',40)
+    //     .attr('height',function(d){
+    //         return h - scaleY(d.country2012)
+    //     });
+    
+    // countries
+    //     .append('text')
+    //     .attr('y', h+20)
+    //     .attr('text-anchor','middle')
+    //     .text(function(d){return d.country});
+
+    // var axisY = d3.axisLeft()
+    //     .scale(scaleY)
+    //     .tickSize(-w-200);
+        
+    // plot.append('g')
+    //     .attr('class','axis axis-y')
+    //     .attr('transform','translate(-100,0')
+    //     .call(axisY);
+    
+    
